7f8698047ce1ab131e546292d31884bd93f9eb20
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-08 00:24:56 +0100

    Ocean Drifter - Boat swaying script

--- Ocean Drifter/Assets/Scripts/BoatSwaying.cs
+++ Ocean Drifter/Assets/Scripts/BoatSwaying.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class BoatSwaying : MonoBehaviour
 {
-    private float swayRotationSpeed;
+    public float swayRotationSpeed;
+    public float rotationZ;
+    public float rotationZ1;
+    public float maxSwayAngle;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        swayRotationSpeed = 2f;
+        maxSwayAngle = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(transform.forward, swayRotationSpeed * Time.deltaTime);
-        if (transform.rotation.z < 1)
+        rotationZ = transform.rotation.eulerAngles.z;
+
+        if (transform.rotation.eulerAngles.z > 5)
         {
-            swayRotationSpeed *= -1;
+            swayRotationSpeed = -Random.Range(2f, 5f);
+            //maxSwayAngle = Random.Range(2f, 5f);
+            Debug.Log("fdssdfsd");
+        }
+        else if (transform.rotation.eulerAngles.z < 360)
+        {
+            swayRotationSpeed = Random.Range(2f, 5f);
+            //maxSwayAngle = Random.Range(2f, 5f);
         }
 
     }
