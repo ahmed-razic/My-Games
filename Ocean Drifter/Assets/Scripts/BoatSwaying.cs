6cfe2d6a3d3193ea9ccb2c71aea4089e27ec3c9b
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-08 00:58:39 -0800

    Added SwayBoat Script

--- Ocean Drifter/Assets/Scripts/BoatSwaying.cs
+++ Ocean Drifter/Assets/Scripts/BoatSwaying.cs
@@ -6,33 +6,35 @@ public class BoatSwaying : MonoBehaviour
 {
     public float swayRotationSpeed;
     public float rotationZ;
-    public float rotationZ1;
-    public float maxSwayAngle;
-
+    //public float rotationZ1;
+    public float swayAngle;
+    public float currentTime;
     // Start is called before the first frame update
     void Start()
     {
-        swayRotationSpeed = 2f;
-        maxSwayAngle = 5f;
+        //swayRotationSpeed = 1f;
+        //transform.Rotate(Vector3.forward, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(transform.forward, swayRotationSpeed * Time.deltaTime);
-        rotationZ = transform.rotation.eulerAngles.z;
+        StartCoroutine(ObjectRotate());
+        transform.Translate(transform.forward * 10 * Time.deltaTime * Input.GetAxis("Vertical"));
+    }
 
-        if (transform.rotation.eulerAngles.z > 5)
-        {
-            swayRotationSpeed = -Random.Range(2f, 5f);
-            //maxSwayAngle = Random.Range(2f, 5f);
-            Debug.Log("fdssdfsd");
-        }
-        else if (transform.rotation.eulerAngles.z < 360)
-        {
-            swayRotationSpeed = Random.Range(2f, 5f);
-            //maxSwayAngle = Random.Range(2f, 5f);
-        }
+    IEnumerator ObjectRotate()
+    {
+        float timer = 0;
+        int maxAngle = 7;
 
+        while (true)
+        {
+            float angle = Mathf.Sin(timer) * maxAngle;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
